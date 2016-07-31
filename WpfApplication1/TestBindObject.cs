96d536953604ab22768b0462f90fd0a21a1d1c24
ruhiel_murrue <ruhiel1983@gmail.com>
2016-07-31 10:24:22 +0900

    初回コミット

---
+++ WpfApplication1/TestBindObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+	using System.ComponentModel;
+	internal class TestBindObject : INotifyPropertyChanged
+	{
+		private int a;
+		private int b;
+
+		public int A
+		{
+			get { return a; }
+			set
+			{
+				a = value;
+				NotifyPropertyChanged("A");
+			}
+		}
+
+		public int B
+		{
+			get { return b; }
+			set
+			{
+				b = value;
+				NotifyPropertyChanged("B");
+			}
+		}
+
+		private void NotifyPropertyChanged(string parameter)
+		{
+			PropertyChanged(this, new PropertyChangedEventArgs(parameter));
+		}
+		public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+	}
+}
